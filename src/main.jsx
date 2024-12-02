72b95e968570b23720ac78b3d346ee66f44bc56e
04_burapa(blue/บลู) <burapa.pratumchai@gmail.com>
2024-12-02 13:30:53 +0700

    s

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import connectDB from './utils/db.js'
-
-
-// เชื่อมต่อ MongoDB พร้อม error handling
-connectDB().catch(error => {
-  console.error('Failed to connect to MongoDB:', error);
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
